06bd3b9bc2fd1434e28cfff9a1452a04643d0a90
vinit01234 <binitsingh2345@gmail.com>
2026-02-06 17:19:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,12 @@
+const messages = [
+  "I love you ❤️",
+  "Bhot jyda gussa krti ho hm to ragebait v ni kiye aur khud se itna jydaaaaaa gusaaa 😡👿",
+  "Ye lo pucchi 😚😚😚😚",
+  "Get well asap anushka 🥺🥺",
+  "hehehehe 😝😛😛😝"
+];
+
 function showLove() {
-  document.getElementById("msg").innerText =
-    "gussa mtt kroo ni to piti piti krunga… I love you ❤️";
+  const msg = messages[Math.floor(Math.random() * messages.length)];
+  document.getElementById("msg").innerText = msg;
 }
